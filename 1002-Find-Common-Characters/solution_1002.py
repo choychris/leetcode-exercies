2ce1267298a45946fd5eef6e60a9f125ca34387e
choychris <choylapchichris@gmail.com>
2021-07-29 18:53:31 +0800

    add another solution to problem 1002

--- 1002-Find-Common-Characters/solution_1002.py
+++ 1002-Find-Common-Characters/solution_1002.py
@@ -16,3 +16,25 @@ class Solution:
                 common.append(c)
 
         return common
+
+
+class SolutionTwo:
+    def commonChars(self, words: list[str]) -> list[str]:
+        count = [float("inf")] * 26
+        common = []
+
+        for w in words:
+            inner_count = [0] * 26
+            for c in w:
+                inner_count[ord(c) - ord("a")] += 1
+
+            for i in range(26):
+                if inner_count[i] < count[i]:
+                    count[i] = inner_count[i]
+
+        for i in range(26):
+            cnt = count[i]
+            for j in range(cnt):
+                common.append(chr(97 + i))
+
+        return common
--- 1002-Find-Common-Characters/test_1002.py
+++ 1002-Find-Common-Characters/test_1002.py
@@ -1,7 +1,12 @@
 from solution_1002 import Solution
+from solution_1002 import SolutionTwo
 
 
 def test():
     assert Solution().commonChars(["cool", "lock", "cook"]) == ["c", "o"]
 
     assert Solution().commonChars(["bella", "label", "roller"]) == ["e", "l", "l"]
+
+    assert SolutionTwo().commonChars(["cool", "lock", "cook"]) == ["c", "o"]
+
+    assert SolutionTwo().commonChars(["bella", "label", "roller"]) == ["e", "l", "l"]
