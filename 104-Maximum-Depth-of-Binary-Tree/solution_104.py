7a43be11b394911192dec5641cf8c7ee0799cffa
choychris <choylapchichris@gmail.com>
2021-08-12 18:48:40 +0800

    complete problem 104

--- 104-Maximum-Depth-of-Binary-Tree/solution_104.py
+++ 104-Maximum-Depth-of-Binary-Tree/solution_104.py
@@ -5,21 +5,70 @@ class TreeNode:
         self.left = left
         self.right = right
 
+
 class Solution:
-    def maxDepth(self, root: list[TreeNode]) -> int:
+    def maxDepth(self, root: TreeNode) -> int:
         if not root:
-          return 0
+            return 0
         if not root.right and not root.left:
-          return 1
-        
+            return 1
+
         def getBranchDepth(node):
             if not node:
-              return 0
+                return 0
 
             depth = 1
             leftDepth = getBranchDepth(node.left)
             rightDepth = getBranchDepth(node.right)
 
             return depth + max(leftDepth, rightDepth)
-          
+
         return getBranchDepth(root)
+
+
+class BFSSolution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+
+        depth = 0
+        queue = [root]
+        while len(queue):
+            depth += 1
+            newQueue = []
+            for v in queue:
+                if v.left:
+                    newQueue.append(v.left)
+                if v.right:
+                    newQueue.append(v.right)
+            queue = newQueue
+
+        return depth
+
+
+class DFSSolution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+
+        depth = 0
+
+        stack = [root]
+
+        while stack:
+            v = stack[-1]
+            if v.left:
+                l = v.left
+                v.left = None
+                stack[-1] = v
+                stack.append(l)
+            elif v.right:
+                r = v.right
+                v.right = None
+                stack[-1] = v
+                stack.append(r)
+            else:
+                depth = max(depth, len(stack))
+                stack.pop()
+
+        return depth
--- 104-Maximum-Depth-of-Binary-Tree/test_104.py
+++ 104-Maximum-Depth-of-Binary-Tree/test_104.py
@@ -1,8 +1,13 @@
 from solution_104 import Solution
+from solution_104 import BFSSolution
+from solution_104 import DFSSolution
 from solution_104 import TreeNode
 
-def tests():
-  assert Solution().maxDepth(TreeNode(1, None, TreeNode(2, None, None)))
 
-  root = TreeNode(3, TreeNode(9), TreeNode(20, TreeNode(15), TreeNode(7)))
-  assert Solution().maxDepth(root) == 3
+def tests():
+    assert Solution().maxDepth(TreeNode(1, None, TreeNode(2, None, None)))
+
+    root = TreeNode(3, TreeNode(9), TreeNode(20, TreeNode(15), TreeNode(7)))
+    assert Solution().maxDepth(root) == 3
+    assert BFSSolution().maxDepth(root) == 3
+    assert DFSSolution().maxDepth(root) == 3
